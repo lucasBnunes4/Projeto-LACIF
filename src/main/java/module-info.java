a0716fcd3ce464768dfd049af2f88c95e588efdf
Lucas <lucasbnunes4@gmail.com>
2025-05-19 18:01:09 -0300

    commit teste

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.projetolacif {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.projetolacif to javafx.fxml;
+    exports org.example.projetolacif;
+}
---
+++ src/main/java/org/example/projetolacif/HomeController.java
@@ -0,0 +1,14 @@
+package org.example.projetolacif;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HomeController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void initialize() {
+        // Código de inicialização se necessário
+    }
+}
